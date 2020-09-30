b9f0b4252496081b390d885b028360a851fd7a4f
Eima1 <e.bakevicius@gmail.com>
2020-09-30 14:48:52 +0300

    3

--- src/Sprint/Shop.java
+++ src/Sprint/Shop.java
@@ -7,10 +7,17 @@ public class Shop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Menu (enter number for actions): " + "\n" + "1. Show merchandise cart." + "\n" + "2. Add goods." + "\n" + "3. Remove goods." + "\n" + "4. Exit shop.");
-        String Goods = sc.next();
+        String read = sc.nextLine();
+        getConsoleObject(read);
     }
 
-    public static Goods getConsoleObject() {
+    public static Goods getConsoleObject(String line) {
+        System.out.println(line);
+        String[] parts = line.split(";");
+        String name = parts[0];
+        int price = Integer.parseInt(parts[1]);
+
+        Goods goods = new Goods(name, price);
         return new Goods();
     }
 }
